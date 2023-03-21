383e477712d8bd6268810c513a950634f01c4efd
Otávio Augusto <tavinteixeira@hotmail.com>
2023-03-21 00:08:11 -0300

    Ex 2.19

---
+++ POO/chapter4/2.19/Vendedor.java
@@ -0,0 +1,27 @@
+public class Vendedor {
+    
+    private String nome;
+    private double venda=0;
+    private double comissao=0.09;
+    private double salarioBase=0;
+
+    public Vendedor(String nome,double salarioBase) {
+        this.salarioBase = salarioBase;
+        this.nome = nome;
+    }
+
+    public void setVenda(double venda) {
+        this.venda += venda;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public void getSalario() {
+        double salario;
+        salario = salarioBase + (venda*comissao);
+        System.out.printf("O vendedor %s vendeu %.2f, e com a comissao recebera %.2f de salario essa semana",nome,venda,salario);
+    }
+
+}
