9c8715940055077e15742fb8e1561799de3f8326
Otavio Augusto Teixeira <tavex@cl.ibilce.unesp.br>
2023-03-23 15:50:36 -0300

    Banco Unesp

---
+++ progExs/bancounesp/Banco.java
@@ -0,0 +1,13 @@
+package progExs.bancounesp;
+
+public class Banco {
+
+    private String name;
+    private String cpf;
+    private double balance;
+    private int accountId;
+    private int agency;
+    private static int count = -1;
+    
+    
+}
